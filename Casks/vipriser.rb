815a28641fc2b5ba93eaa5b6aacc0e4c7f215950
d-salt <d.salt.web+dev@gmail.com>
2019-07-24 14:05:56 +0900

    first commit

---
+++ Casks/vipriser.rb
@@ -0,0 +1,10 @@
+cask 'vipriser' do
+  version '2.1'
+  sha256 '99be64efc191428e8771a5946216374c71375ed994c466586a4cbfa13c1d2107'
+
+  url "https://www.dropbox.com/s/5mmkdqxey8dr3n2/VipRiser-2.1.zip?dl=1"
+  name 'VipRiser'
+  homepage 'https://onflapp.wordpress.com'
+
+  app 'VipRiser.app'
+end
